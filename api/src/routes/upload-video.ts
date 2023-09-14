a8384007850df11099887d0dff1b8acbf77d88b2
JrzenonDev <jrobertoonb@gmail.com>
2023-09-13 23:34:40 -0300

    feat: creating route upload-video using apis nodejs and package fastify multipart

---
+++ api/src/routes/upload-video.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { fastifyMultipart } from "@fastify/multipart";
+import path from "node:path";
+import { randomUUID } from "node:crypto";
+import fs from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+const pump = promisify(pipeline);
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1_048_576 * 25, // 25MB
+    },
+  });
+
+  app.post("/videos", async (request, replay) => {
+    const data = await request.file();
+
+    if (!data) {
+      return replay.status(400).send({
+        error: "Missing file input.",
+      });
+    }
+
+    const extension = path.extname(data.filename);
+
+    if (extension !== ".mp3") {
+      return replay.status(400).send({
+        error: "Invalid input type, please upload a mp3 file.",
+      });
+    }
+
+    const fileBaseName = path.basename(data.filename, extension);
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`;
+
+    const uploadDestination = path.resolve(
+      __dirname,
+      "../../temp",
+      fileUploadName
+    );
+
+    await pump(data.file, fs.createWriteStream(uploadDestination));
+
+    return replay.send();
+  });
+}
