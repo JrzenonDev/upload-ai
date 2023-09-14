0622cbd819a6d3df6356e83e1eded88cdc0fea2c
JrzenonDev <jrobertoonb@gmail.com>
2023-09-14 01:33:39 -0300

    feat: creating route generate-ai-completion

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,12 +2,14 @@ import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscription } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscription);
+app.register(generateAICompletionRoute);
 
 app
   .listen({
